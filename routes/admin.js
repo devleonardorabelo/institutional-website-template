0fc8dc492683a55632c2be89049bfe79ade5c017
deleonardorabelo <leonardomrabelo@gmail.com>
2020-01-15 10:02:18 -0300

    Basic changes

--- routes/admin.js
+++ routes/admin.js
@@ -10,46 +10,51 @@ async function verifyUser(req, res, next) {
         return res.redirect('/auth/login')
     }
 }
-router.get('/', verifyUser, (req, res) => {
-    res.render('admin/home', {
-        user: req.user
+//INDEX ROUTES
+    router.get('/', verifyUser, (req, res) => {
+        res.render('admin/home', {
+            user: req.user
+        })
     })
-})
-router.get('/posts', verifyUser, async (req, res) => {
-    let posts = await Post.find()
-    res.render('admin/posts', {
-        user: req.user,
-        post: posts
+//POST ROUTES
+    router.get('/posts', verifyUser, async (req, res) => {
+        let posts = await Post.find()
+        res.render('admin/posts', {
+            user: req.user,
+            post: posts
+        })
     })
-})
-router.post('/posts/add', verifyUser, async (req, res) => {
-    let findPost = await Post.findOne({url: req.body.url})
-    if(findPost){
-        res.redirect('/admin/posts')
-        console.log('já existe essa url')
-    }else{
-        new Post(req.body).save()
+    router.post('/addpost', verifyUser, async (req, res) => {
+        let findPost = await Post.findOne({url: req.body.url})
+        if(findPost){
+            res.redirect('/admin/posts')
+            console.log('já existe essa url')
+        }else{
+            new Post(req.body).save()
+            return res.redirect('/admin/posts')
+        }
+    })
+    router.get('/delpost/:id', verifyUser, async (req, res) => {
+        await Post.deleteOne({_id: req.params.id})
         return res.redirect('/admin/posts')
-    }
-})
-
-router.get('/messages', verifyUser, async (req, res) => {
-    let messages = await Message.find()
-    res.render('admin/messages', {
-        user: req.user,
-        message: messages
     })
-})
-
-router.get('/messages', verifyUser, async (req, res) => {
-    let messages = await Message.find()
-    res.render('admin/messages', {
-        user: req.user,
-        message: messages
+//MESSAGE ROUTES
+    router.get('/messages', verifyUser, async (req, res) => {
+        let messages = await Message.find()
+        res.render('admin/messages', {
+            user: req.user,
+            message: messages
+        })
     })
-})
-router.get('/delmsg/:id', verifyUser, async (req, res) => {
-    let delmsg = await Message.deleteOne({_id: req.params.id})
-    return res.redirect('/admin/messages')
-})
+    router.get('/messages', verifyUser, async (req, res) => {
+        let messages = await Message.find()
+        res.render('admin/messages', {
+            user: req.user,
+            message: messages
+        })
+    })
+    router.get('/delmsg/:id', verifyUser, async (req, res) => {
+        await Message.deleteOne({_id: req.params.id})
+        return res.redirect('/admin/messages')
+    })
 module.exports = router
--- routes/auth.js
+++ routes/auth.js
@@ -5,46 +5,43 @@ const express  = require('express'),
 	  require('../config/passport')
 	  
 // AUTH CHECK
-function isLoggedIn(req, res, next) {
-	if (req.isAuthenticated())
-		return next();
-	res.redirect('/login');
-}
-
+	function isLoggedIn(req, res, next) {
+		if (req.isAuthenticated())
+			return next();
+		res.redirect('/login');
+	}
 // LOGIN 
-router.get('/login', async (req, res) => {
-	let findUser = await User.findOne()
-	if(!findUser)
-		return res.redirect('/auth/signup')
+	router.get('/login', async (req, res) => {
+		let findUser = await User.findOne()
+		if(!findUser)
+			return res.redirect('/auth/signup')
 
-	res.render('login', { message: req.flash('loginMessage') });
-});
-
+		res.render('login', { message: req.flash('loginMessage') });
+	});
 // POST LOGIN
-router.post('/login', passport.authenticate('local-login', {
-	successRedirect : '/admin',
-	failureRedirect : '/auth/login',
-	failureFlash : true
-}));
-
+	router.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/admin',
+		failureRedirect : '/auth/login',
+		failureFlash : true
+	}));
 //SIGNUP 
-router.get('/signup', async (req, res) => {
-	let findUser = await User.findOne()
-	if(findUser)
-		return res.redirect('/auth/login')
-		
-	res.render('signup', { message: req.flash('signupMessage') });
-});
-
+	router.get('/signup', async (req, res) => {
+		let findUser = await User.findOne()
+		if(findUser)
+			return res.redirect('/auth/login')
+			
+		res.render('signup', { message: req.flash('signupMessage') });
+	});
 // POST SIGNUP
-router.post('/signup', passport.authenticate('local-signup', {
-	successRedirect : '/admin',
-	failureRedirect : '/auth/signup',
-	failureFlash : true
-}));
+	router.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/admin',
+		failureRedirect : '/auth/signup',
+		failureFlash : true
+	}));
 //LOGOUT
 router.get('/logout', (req, res) => {
 	req.logout();
 	res.redirect('/auth/login');
 });
+
 module.exports = router
